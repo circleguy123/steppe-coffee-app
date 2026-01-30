24e12995730979a33928a0eafb42fb19de379523
Nurlan <lowkigod@gmail.com>
2026-01-30 12:25:20 +0500

    Enable CORS for admin panel

--- api/src/main.ts
+++ api/src/main.ts
@@ -8,13 +8,16 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://admin.steppecoffee.kz'],
+    credentials: true,
+  });
+  
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-
   app.useGlobalPipes(new ValidationPipe(validatorOptions));
-
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-
   await app.listen(3000);
 }
 bootstrap();
